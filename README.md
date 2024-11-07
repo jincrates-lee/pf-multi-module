7adfb56565b13ef46d9cc64a343df968575514ac
jincrates <94jingyu@gmail.com>
2024-11-07 16:08:11 +0900

    docs: ReadMe 이미지 변경2

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## 모듈 의존성 그래프
 
 ![module_graph-20241107.png](docs%2Fimages%2Fmodule_graph-20241107.png)
+
+- 위에 처럼 하면 api 모듈에서 bootJar를 만들 수가 없다. 다른 모듈에 대한 의존성이 없기 때문
+
+api 모듈에서 bootJar를 만들기 위해서는 다음과 같이 설정해야 한다.
+![module_graph-20241107-1600.png](docs%2Fimages%2Fmodule_graph-20241107-1600.png)
--- bootstrap/api/src/main/java/me/jincrates/pf/api/ApiRunner.java
+++ bootstrap/api/src/main/java/me/jincrates/pf/api/ApiRunner.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(basePackages = "me.jincrates.pf.infrastructure.dataaccess")
-@EnableJpaRepositories(basePackages = "me.jincrates.pf.infrastructure.dataaccess")
 @ComponentScan(basePackages = "me.jincrates.pf")
 @SpringBootApplication
 public class ApiRunner {
